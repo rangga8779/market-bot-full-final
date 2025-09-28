c59945d1878ad7bc3c7dd392fa833ba6e1ca1c22
rangga8779 <naufalrangga753@gmail.com>
2025-09-28 11:56:52 +0700

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  BOT_TOKEN: "8356664071:AAF7gedAa3TlYDhW7ZV55t1AomNIazn6p3Q",
+  ADMIN_ID: "7056257981",  // ganti pakai chat_id kamu
+
+  // Pembayaran
+  DANA_NUMBER: "081261959886",
+  QR_IMAGE: "https://files.catbox.moe/fp37yy.jpg",   // QR pembayaran (link catbox/telegra.ph)
+
+  // Foto Start
+  START_IMAGE: "https://files.catbox.moe/wwmlye.jpg", // foto catbox / banner start
+
+  // Umum
+  CURRENCY: "Rp"
+};
